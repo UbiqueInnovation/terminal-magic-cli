c44b95f191db1e80db54b111ded9badc7942c29c
Patrick Amrein <amrein@ubique.ch>
2020-05-25 10:07:49 +0200

    add structure for coonfig

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use toml;
+use serde_derive::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+
+fn main() -> Result<(), std::io::Error>{
+    let input : PluginInfo = toml::from_str(&std::fs::read_to_string("plugintest.toml")?).unwrap();
+    if let Some(inner) = input.placeholders.get("DATABASES") {
+        println!("Found databases is type : {}", inner.placeholder_type)
+    }
+   Ok(())
+}
+
+#[derive(Deserialize, Serialize)]
+struct PluginInfo {
+    plugin_info : Package,
+    placeholders : BTreeMap<String, Placeholder>
+}
+
+#[derive(Deserialize, Serialize)]
+struct Package {
+    author : String,
+    version : String,
+    plugin_type : PluginType 
+}
+
+#[derive(Deserialize, Serialize)]
+enum PluginType {
+    Shell(String),
+    Script(String)
+}
+
+#[derive(Deserialize, Serialize)]
+struct Placeholder {
+    placeholder_type : String
 }
