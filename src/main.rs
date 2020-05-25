ebc41df091a0e450184360e73daa9bac677c0319
Patrick Amrein <amrein@ubique.ch>
2020-05-25 16:36:33 +0200

    add remove and add commands

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ use clap::App;
 
 use toml;
 use serde_derive::{Deserialize, Serialize};
-use std::collections::{BTreeMap, HashMap};
+use std::collections::{BTreeMap};
 use mustache::MapBuilder;
 use prompts::{text::TextPrompt, confirm::ConfirmPrompt,Prompt};
 use async_std::task;
-use linked_hash_map::LinkedHashMap;
+use std::path::Path;
+use dirs::home_dir;
 
 static CONFIG_DIR : &str = ".terminal-magic";
 
@@ -16,11 +17,12 @@ fn main() {
     let yaml = load_yaml!("cli.yaml");
     let app = App::from_yaml(yaml);
     let matches = app.get_matches();
+    let git_repo = matches.value_of("git_repo").unwrap();
     match matches.subcommand_name() {
         Some("install") => {
             if let Some(install_cmd) = matches.subcommand_matches("install") {
                 if let Some(plugin_name) = install_cmd.value_of("INPUT") {
-                    install(plugin_name);
+                    install(git_repo,plugin_name);
                 } else {
                     eprintln!("{}", matches.usage());
                     std::process::exit(1);
@@ -32,32 +34,105 @@ fn main() {
             }
         },
         Some("update") => {
+            if let Some(install_cmd) = matches.subcommand_matches("update") {
+                if let Some(plugin_name) = install_cmd.value_of("INPUT") {
+                    update(git_repo,plugin_name);
+                } else {
+                    eprintln!("{}", matches.usage());
+                    std::process::exit(1);
+                }
+            }
+            else {
+                eprintln!("{}", matches.usage());
+                std::process::exit(1);
+            }
         },
-        Some("remove") => {},
+        Some("remove") => {
+            if let Some(install_cmd) = matches.subcommand_matches("remove") {
+                if let Some(plugin_name) = install_cmd.value_of("INPUT") {
+                    remove(plugin_name);
+                } else {
+                    eprintln!("{}", matches.usage());
+                    std::process::exit(1);
+                }
+            }
+            else {
+                eprintln!("{}", matches.usage());
+                std::process::exit(1);
+            }
+        },
         _ => {
             let mut out = std::io::stdout();
             eprintln!("{}", matches.usage());
             std::process::exit(1);
         }
     }
-
-    let database_mustache = mustache::compile_str(&std::fs::read_to_string("database.sh").unwrap()).unwrap();
-    let mut plugin_test : PluginInfo = toml::from_str(&std::fs::read_to_string("plugintest.toml").unwrap()).unwrap();
-    let mut mustache_map = MapBuilder::new();
-    for mut placeholder in plugin_test.placeholders.iter_mut() {
-        println!("Read {}", placeholder.0);
-        read(&mut placeholder.1);
-        mustache_map = mustache_map.insert(placeholder.0, &placeholder.1).expect("Could not parse object");
-    }
-    let map = mustache_map.build();
-
-    println!("{:?}",plugin_test);
-    println!("{:?}",toml::to_string(&plugin_test));
-    //database_mustache.render_data(&mut std::io::stdout(), &map).unwrap();
 }
 
-fn install(plugin_name : &str) {
-    
+fn update(git_repo : &str, plugin_name : &str) {
+    let home_path = home_dir().expect("Home dir not fount").join(CONFIG_DIR).join(plugin_name);
+    if !home_path.exists() {
+        eprintln!("module is not installed");
+        std::process::exit(1);
+    }
+    let path_to_module = Path::new(git_repo).join(plugin_name);
+    let mustache = mustache::compile_path(path_to_module.join("template.sh")).expect("Could not parse mustache template");
+
+    let toml_str = std::fs::read_to_string(home_path.join("data.toml")).expect("Could not find config.toml");
+    let mut toml : PluginInfo = toml::from_str(&toml_str).expect("Cannot parse TOML");
+    let mut mustache_map_builder = MapBuilder::new();
+    for mut placeholder in toml.placeholders.iter_mut() {
+        mustache_map_builder = mustache_map_builder.insert(placeholder.0, &placeholder.1).expect("Could not parse object");
+    }
+    let mustache_map = mustache_map_builder.build();
+    render(toml, mustache, mustache_map, plugin_name);
+}
+
+fn remove(plugin_name : &str) {
+    let home_path = home_dir().expect("Home dir not found").join(CONFIG_DIR).join(plugin_name);
+    if !home_path.exists() {
+        return;
+    }
+    std::fs::remove_dir_all(home_path).expect("Could not remove directory");
+}
+
+fn install(git_repo : &str, plugin_name : &str) {
+    let home_path = home_dir().expect("Home dir not fount").join(CONFIG_DIR).join(plugin_name);
+    if home_path.exists() {
+        return;
+    }
+    let path_to_module = Path::new(git_repo).join(plugin_name);
+    let mustache = mustache::compile_path(path_to_module.join("template.sh")).expect("Could not parse mustache template");
+    let toml_str = std::fs::read_to_string(path_to_module.join("config.toml")).expect("Could not find config.toml");
+    let mut toml : PluginInfo = toml::from_str(&toml_str).expect("Cannot parse TOML");
+    let mut mustache_map_builder = MapBuilder::new();
+    for mut placeholder in toml.placeholders.iter_mut() {
+        println!("Read {}", placeholder.0);
+        read(&mut placeholder.1);
+        mustache_map_builder = mustache_map_builder.insert(placeholder.0, &placeholder.1).expect("Could not parse object");
+    }
+    let mustache_map = mustache_map_builder.build();
+    render(toml, mustache, mustache_map, plugin_name);
+}
+
+fn render(mut toml : PluginInfo, mustache : mustache::Template, mustache_map : mustache::Data, plugin_name : &str) {
+    let home_path = home_dir().expect("Home dir not fount").join(CONFIG_DIR).join(plugin_name);
+   
+    if let Ok(_) = std::fs::create_dir_all(&home_path) {
+        println!("Created directory");
+    }
+    if let Ok(_) = std::fs::remove_file(home_path.join("script.sh")){
+        println!("script.sh already existed");
+    }
+    let mut script_file = std::fs::File::create(home_path.join("script.sh")).expect("Could not create file");
+    if let Ok(_) = mustache.render_data(&mut script_file, &mustache_map) {
+        if let Ok(_) = std::fs::remove_file(home_path.join("data.toml")) {
+            println!("data.toml File existed");
+        }
+        if let Ok(_) = std::fs::write(home_path.join("data.toml"),toml::to_vec(&toml).expect("could not serialize data")){
+            println!("Successfully wrote plugin {}!", plugin_name);
+        }
+    }   
 }
 
 fn read(entry_type : &mut EntryType) {
